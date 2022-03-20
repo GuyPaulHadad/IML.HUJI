4b6b2041f7a2b4b8b5ca6e3d3d9597285f4815a3
GuyPaulHadad <62478358+GuyPaulHadad@users.noreply.github.com>
2022-03-20 12:23:26 +0200

    gaussian update

--- IMLearn/learners/gaussian_estimators.py
+++ IMLearn/learners/gaussian_estimators.py
@@ -6,6 +6,7 @@ import numpy as np
 import scipy
 from numpy.linalg import inv, det, slogdet
 from scipy.stats import *
+import scipy.stats
 import plotly
 import plotly.express as px
 from plotly import graph_objects as go
@@ -118,9 +119,8 @@ class UnivariateGaussian:
         # log in base of e. mu - expectation. sigma - standard deviation
 
         sample_amount = X.size
-        sum_of_normal_exp_power = 0
-        for i in range(sample_amount):
-            sum_of_normal_exp_power = sum_of_normal_exp_power - 0.5 * ((X[i] - mu) / sigma) ** 2
+        sum_of_normal_exp_power = np.sum(-0.5*((X-mu)/sigma)**2)
+
         return sample_amount * math.log((1.0 / (sigma * math.sqrt(2 * math.pi))), math.e) + sum_of_normal_exp_power
 
 
@@ -254,101 +254,3 @@ class MultivariateGaussian:
             raise ValueError("Cov matrix determinant cannot equal zero")
 
 
-if __name__ == '__main__':
-    """
-    normal1 = UnivariateGaussian();
-    print(str(normal1.mu_) + " " + str(normal1.var_))
-    numpyArr = np.random.normal(10, 1, 1000)
-    normal1.fit(numpyArr)
-    print("Estimated mu: " + str(normal1.mu_) + " | Estimated cov: " + str(normal1.var_))
-    # normal1.pdf(normal1)
-    print(numpyArr.size)
-    ms = np.linspace(5, numpyArr.size, round(numpyArr.size / 5)).astype(np.int)
-    absolute_mean_error = []
-
-    for m in ms:
-        absolute_mean_error.append(abs(normal1.mu_ - np.mean(numpyArr[1:m + 1])))
-
-    go.Figure([go.Scatter(x=ms, y=absolute_mean_error, mode='markers+lines', name=r"text{$\mu-error$}"),
-               go.Scatter(x=ms, y=[0] * len(ms), mode='lines', name=r"$no-error$")],
-              layout=go.Layout(title=r"$\text{Absolute Mean Error as a Function of Number of Samples}$",
-                               xaxis_title="$m\\text{ - number of samples}$",
-                               yaxis_title="r$\\text{Absolute Mean Error}$",
-                               height=400)).show()
-    pdf_array = normal1.pdf(numpyArr)
-    empirical_pdf = np.full(1000, 1 / 1000)
-
-    go.Figure(
-        [go.Scatter(x=numpyArr, y=pdf_array, mode='markers')],
-        layout=go.Layout(
-            title=r"$" + "\\text{Density Function of a normal distribution: }" + "\mu=" + "{:.2f}".format(
-                normal1.mu_) + ", \sigma^2=" + "{:.2f}".format(normal1.var_) + "$",
-            xaxis_title="$Value of Samples$",
-            yaxis_title="r$\\text{Probability Density}$",
-            height=400)).show()
-    print(normal1.log_likelihood(10, 1, numpyArr))
-
-    """
-    mu_multivariate = np.array([0, 0, 4, 0])
-    cov_multivariate = np.array([[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
-    # print(cov_multivariate)
-    multivariate_samples = np.random.multivariate_normal(mu_multivariate, cov_multivariate, 1000)
-    # print(multivariate_samples)
-    # multivariate_gaussian = MultivariateGaussian()
-    # multivariate_gaussian.fit(multivariate_samples)
-    # print(multivariate_gaussian.mu_)
-    # print("")
-    # print(multivariate_gaussian.cov_)
-    # print(multivariate_samples[0])
-    # print(multivariate_gaussian.cov_)
-    # print(multivariate_gaussian.pdf(multivariate_samples[0]))
-    # multi = scipy.stats.multivariate_normal(multivariate_gaussian.mu_,
-    # multivariate_gaussian.cov_)
-    # print(multi.pdf(multivariate_samples[0]))
-    multi = MultivariateGaussian()
-    # multi.mu_ = np.array([0, 0, 4, 0])
-    # multi.cov_ = np.array([[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
-    multi.fit(multivariate_samples)
-    # check = np.tile([0,0,4,0],(1000,1))
-
-    # multi.fitted_ = True
-
-    # print(multivariate_normal.pdf(multivariate_samples,multi.mu_,multi.cov_)-multi.pdf(multivariate_samples))
-    """
-    ls = np.linspace(-10, 10, 200)
-    arr = np.zeros((200,200))
-    for i in range(200):
-        counter = 0
-        for j in range(50):
-
-            arr[i][counter] = ls[i]
-            arr[i][counter+2] = ls[j]
-            counter = counter+4
-    print(arr)
-    """
-    # test1 = np.array([[1,2,3,4],[1,2,3,4],[4,5,6,7]])
-    # test2 = np.array([[4,5,6,7],[1,2,3,4],[1,2,3,4]])
-    # print(np.sum(test1*test2))
-    multi_scipy = multivariate_normal(multi.mu_, multi.cov_)
-    f1 = f3 = np.linspace(-10, 10, 200)
-
-    likelihood_arr = np.zeros((200, 200))
-    max_mu = np.array([0, 0, 0, 0])
-    max_likelihood_value = multi.log_likelihood(max_mu, cov_multivariate, multivariate_samples)
-
-    for i in range(200):
-        for j in range(200):
-            cur_mu = np.array([f1[i], 0, f3[j], 0])
-            likelihood_value = multi.log_likelihood(cur_mu, multi.cov_, multivariate_samples)
-            likelihood_arr[i][j] = likelihood_value
-            if max_likelihood_value < likelihood_value:
-                max_mu = cur_mu
-                max_likelihood_value = likelihood_value
-
-    print(max_likelihood_value)
-    print(max_mu)
-    fig3 = px.imshow(likelihood_arr, x=f1, y=f3, title="Log - likelihood pertaining different expectation values",
-                     height=450,
-                     labels=dict(x="Value of F1", y="Value of F3", color="Log - Likelihood Value"),
-                     color_continuous_scale="Hot")
-    plotly.offline.plot(fig3)
--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -2,29 +2,87 @@ from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+import plotly
+import plotly.express as px
 pio.templates.default = "simple_white"
+
 
 
 def test_univariate_gaussian():
     # Question 1 - Draw samples and print fitted model
-    raise NotImplementedError()
+    np.random.seed(0)
+    normal1 = UnivariateGaussian();
+    numpy_normal_samples = np.random.normal(10, 1, 1000)
+    normal1.fit(numpy_normal_samples)
+    print("("+str(normal1.mu_)+","+str(normal1.var_)+")")
+    #print("Estimated mu: " + str(normal1.mu_) + " | Estimated var: " + str(normal1.var_))
+
+
 
     # Question 2 - Empirically showing sample mean is consistent
-    raise NotImplementedError()
+    ms = np.linspace(5, numpy_normal_samples.size, round(numpy_normal_samples.size / 5)).astype(np.int)
+    absolute_mean_error = []
+    for m in ms:
+        absolute_mean_error.append(abs(normal1.mu_ - np.mean(numpy_normal_samples[1:m + 1])))
+
+    go.Figure([go.Scatter(x=ms, y=absolute_mean_error, mode='markers+lines', name=r"text{$\mu-error$}"),
+               go.Scatter(x=ms, y=[0] * len(ms), mode='lines', name=r"$no-error$")],
+              layout=go.Layout(title=r"$\text{Absolute Mean Error as a Function of Number of Samples}$",
+                               xaxis_title="$m\\text{ - number of samples}$",
+                               yaxis_title="r$\\text{Absolute Mean Error}$",
+                               height=400)).show()
+
 
     # Question 3 - Plotting Empirical PDF of fitted model
-    raise NotImplementedError()
+    pdf_array = normal1.pdf(numpy_normal_samples)
+    go.Figure(
+        [go.Scatter(x=numpy_normal_samples, y=pdf_array, mode='markers')],
+        layout=go.Layout(
+            title=r"$" + "\\text{Density Function of an Estimated Normal Distribution: }" + "\mu=" + "{:.2f}".format(
+                normal1.mu_) + ", \sigma^2=" + "{:.2f}".format(normal1.var_) + "$",
+            xaxis_title="$Value of Samples$",
+            yaxis_title="r$\\text{Probability Density}$",
+            height=400)).show()
+    print(normal1.log_likelihood(10, 1, numpy_normal_samples))
 
 
 def test_multivariate_gaussian():
     # Question 4 - Draw samples and print fitted model
-    raise NotImplementedError()
+    mu_multivariate = np.array([0, 0, 4, 0])
+    cov_multivariate = np.array([[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
+    multivariate_samples = np.random.multivariate_normal(mu_multivariate, cov_multivariate, 1000)
+    multi = MultivariateGaussian()
+    multi.fit(multivariate_samples)
+    print(multi.mu_)
+    print(multi.cov_)
 
     # Question 5 - Likelihood evaluation
-    raise NotImplementedError()
+    f1 = f3 = np.linspace(-10, 10, 200)
+
+    likelihood_arr = np.zeros((200, 200))
+    max_likelihood_value = -100000000
+
+    for i in range(200):
+        for j in range(200):
+            cur_mu = np.array([f1[i], 0, f3[j], 0])
+            likelihood_value = multi.log_likelihood(cur_mu, multi.cov_, multivariate_samples)
+            likelihood_arr[i][j] = likelihood_value
+
+    fig3 = px.imshow(likelihood_arr, x=f1, y=f3, title="Log - likelihood pertaining different expectation values",
+                     height=450,
+                     labels=dict(x="Value of F1", y="Value of F3", color="Log - Likelihood Value"),
+                     color_continuous_scale="Hot")
+    plotly.offline.plot(fig3)
+
+
+
+
 
     # Question 6 - Maximum likelihood
-    raise NotImplementedError()
+    ans = np.unravel_index(np.argmax(likelihood_arr, axis=None), likelihood_arr.shape)
+    print("f1: " + str(f1[ans[0]]))
+    print("f3: " + str(f3[ans[1]]))
+    print("Max Likelihood: "+ str(likelihood_arr[ans[0]][ans[1]]))
 
 
 if __name__ == '__main__':
